e7f3bc4ecccb53f0cec04e820ee6cde740423ca9
gatiskevans <gatis.kevans@gmail.com>
2021-02-28 22:45:12 +0200

    Pirmais

---
+++ src/Gatis.java
@@ -0,0 +1,13 @@
+public class Gatis {
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                Laba, ļoti laba diena. Laikam.\s
+                 Mani sauc Gatītis, un man ir 27 gadiņi\s
+                Man garšo ēst, dzert un gulēt. A ko tu lasi vispār?! A?\s
+                 Atā!""");
+
+    }
+
+}
---
+++ src/Gatis/MyClass.java
@@ -0,0 +1,14 @@
+package Gatis;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("""     
+                Hello world!\s
+                Mani sauc Gatis Ķanavonvons!\s
+                Bye bye hehe!!""");
+
+    }
+
+}
