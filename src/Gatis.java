9cdeeff5d4d0221807a20a9ff043d94f2cf768ab
gatiskevans <gatis.kevans@gmail.com>
2021-02-28 23:14:54 +0200

    src/Gatis.java

--- src/Gatis.java
+++ src/Gatis.java
@@ -3,10 +3,9 @@ public class Gatis {
     public static void main(String[] args) {
 
         System.out.println("""
-                Laba, ļoti laba diena. Laikam.\s
-                 Mani sauc Gatītis, un man ir 27 gadiņi\s
-                Man garšo ēst, dzert un gulēt. A ko tu lasi vispār?! A?\s
-                 Atā!""");
+                Šī ir default klase.\s
+                Nav iekļauta packages.\s
+                Bye""");
 
     }
 
