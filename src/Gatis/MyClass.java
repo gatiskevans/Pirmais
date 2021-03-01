474b5541ff50a7bd5b3ea786044756f064f86b9f
gatiskevans <gatis.kevans@gmail.com>
2021-03-01 23:41:52 +0200

    .

--- src/Gatis/MyClass.java
+++ src/Gatis/MyClass.java
@@ -1,5 +1,11 @@
 package Gatis;
 
+//Nu labi, iekomentēšu šito ten
+
+/*Ja jau tik ļoti gribi,
+tad iekomentēšu arī blokā!
+ */
+
 public class MyClass {
 
     public static void main(String[] args) {
