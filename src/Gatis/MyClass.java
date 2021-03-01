e3b466bdd20c3469955ed1a7aeb9502e996e9efe
gatiskevans <gatis.kevans@gmail.com>
2021-03-01 23:53:09 +0200

    .

--- src/Gatis/MyClass.java
+++ src/Gatis/MyClass.java
@@ -1,9 +1,10 @@
 package Gatis;
 
-//Nu labi, iekomentēšu šito ten
+//Šis ir vienas rindas komentārs
 
-/*Ja jau tik ļoti gribi,
-tad iekomentēšu arī blokā!
+/*Šis ir bloka komentārs,
+kuru var rakstīt vairākās rindās,
+līdz pievieno beigu komandu.
  */
 
 public class MyClass {
@@ -14,6 +15,11 @@ public class MyClass {
                 Hello world!\s
                 Mani sauc Gatis Ķevans!\s
                 Bye bye hehe!!""");
+
+        String pirmais = "Šis ir";
+        String otrais = " mans teksts!";
+
+        System.out.println(pirmais + otrais);
 
     }
 
